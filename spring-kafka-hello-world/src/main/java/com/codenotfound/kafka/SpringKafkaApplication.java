bc61d4804c05fe78622344c4e81b21dff1849eb4
Cesar Rios Torres <cesar.rios@mdp.com.pe>
2020-12-11 18:42:49 -0500

    configuracion de cola kafka

--- spring-kafka-hello-world/src/main/java/com/codenotfound/kafka/SpringKafkaApplication.java
+++ spring-kafka-hello-world/src/main/java/com/codenotfound/kafka/SpringKafkaApplication.java
@@ -1,12 +1,42 @@
 package com.codenotfound.kafka;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.ListTopicsResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class SpringKafkaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringKafkaApplication.class, args);
+
+      Properties properties = new Properties();
+      properties.put("bootstrap.servers", "srvbossqa.vntsasqa.lan:9092");
+      properties.put("connections.max.idle.ms", 10000);
+      properties.put("request.timeout.ms", 5000);
+      try (AdminClient client = KafkaAdminClient.create(properties))
+      {
+          ListTopicsResult topics = client.listTopics();
+          Set<String> names = topics.names().get();
+          if (names.isEmpty())
+          {
+              // case: if no topic found.
+              System.out.println("No topic found");
+          }
+
+          System.out.println("Topic founded: " + names.toString());
+
+      }
+      catch (InterruptedException | ExecutionException e)
+      {
+          // Kafka is not available
+          System.err.println("Kakfka is not available");
+      }
   }
 }
